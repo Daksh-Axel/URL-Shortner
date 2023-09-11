235c1fe9dc8b8923d0beb365fa3a476a9f1874c7
Daksh-Axel <dakshrajsadashiv@iitbhilai.ac.in>
2023-09-12 04:47:57 +0530

    dockerize

--- api/main.go
+++ api/main.go
@@ -9,7 +9,7 @@ import(
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
 	"github.com/Daksh-Axel/shorten-url-fiber-redis/routes"
-	
+	"github.com/ansrivas/fiberprometheus/v2"
 )
 
 func setupRoutes(app *fiber.App){
@@ -23,6 +23,12 @@ func main(){
 		fmt.Println(err)
 	}
 	app := fiber.New()
+	
+	// adding middleware: Exporter for prometheus
+	prometheus := fiberprometheus.New("my-service-name")
+  	prometheus.RegisterAt(app, "/metrics")
+ 	app.Use(prometheus.Middleware)
+ 	
 	app.Use(logger.New())
 	setupRoutes(app)
 	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
